53fbdf973caadb34b5938d7e1f1adfa168a6aeaf
JaeUpSu <hyeongs2323@gmail.com>
2022-11-24 19:20:42 +0900

    feat(domain): 이동 방향 입력했을 때 상태값 반환 기능 추가

--- src/main/java/bridge/domain/MoveAble.java
+++ src/main/java/bridge/domain/MoveAble.java
@@ -2,28 +2,39 @@ package bridge.domain;
 
 import java.util.Arrays;
 import java.util.function.BiPredicate;
+import java.util.stream.Stream;
 
 public enum MoveAble {
-    ALLOWANCE("O", (input, moving) -> input.equals(moving.getType())),
-    DIS_ALLOWANCE("X", (input, moving) -> input.equals(moving.getType())),
-    EMPTY(" ", (input, moving) -> !input.equals(moving.getType()));
+    ALLOWANCE("O", (input, moving) -> input.equals(moving.getType())
+            , (input, moving) -> input.equals(moving.getType())),
+    DIS_ALLOWANCE("X", (input, moving) -> true
+            , (input, moving) -> !input.equals(moving.getType())),
+    EMPTY(" ", (input, moving) -> !input.equals(moving.getType())
+            , (input, moving) -> input.equals(moving.getType()));
 
     private final String status;
-    private final BiPredicate<String, MovingType> isMatch;
+    private final BiPredicate<String, MovingType> isBridgeMatch;
+    private final BiPredicate<String, MovingType> isTypeMatch;
 
-    MoveAble(String status, BiPredicate<String, MovingType> isMatch) {
+    MoveAble(String status, BiPredicate<String, MovingType> isBridgeMatch, BiPredicate<String, MovingType> isTypeMatch) {
         this.status = status;
-        this.isMatch = isMatch;
+        this.isBridgeMatch = isBridgeMatch;
+        this.isTypeMatch = isTypeMatch;
     }
 
-    public static String getFilter(String input, MovingType movingType) {
-        return Arrays.stream(MoveAble.values())
-                .filter(move -> move.isMatch.test(input, movingType))
+    public static String getMoveBridgeFilter(String input, MovingType movingType, MovingType correctType) {
+        Stream<MoveAble> moveAbleStream = Arrays.stream(MoveAble.values())
+                .filter(move -> move.isBridgeMatch.test(input, movingType));
+
+        return getMovingTypeFilter(moveAbleStream, input, correctType);
+    }
+
+    private static String getMovingTypeFilter(Stream<MoveAble> moves, String input, MovingType correctType) {
+        return moves.filter(move -> move.isTypeMatch.test(input, correctType))
                 .findAny()
                 .orElse(EMPTY)
                 .getStatus();
     }
-
     private String getStatus() {
         return status;
     }
