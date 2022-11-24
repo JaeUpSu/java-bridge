4705f156b6a21f621dbc47f027ae1ad0e659ea6e
JaeUpSu <hyeongs2323@gmail.com>
2022-11-24 16:51:00 +0900

    feat(validate): 이동 방향으로 적합한지 검증 기능 정의

---
+++ src/main/java/bridge/validate/validateMovingType.java
@@ -0,0 +1,18 @@
+package bridge.validate;
+
+import bridge.domain.Moving;
+import bridge.exception.InvalidMovingTypeException;
+
+public class validateMovingType {
+    public static void validate(String inputValue) {
+        Moving MovingUp = Moving.UP;
+        Moving MovingDown = Moving.DOWN;
+
+        if (inputValue.equals(MovingUp.getType())
+                || inputValue.equals(MovingDown.getType())) {
+            return;
+        }
+
+        throw new InvalidMovingTypeException();
+    }
+}
