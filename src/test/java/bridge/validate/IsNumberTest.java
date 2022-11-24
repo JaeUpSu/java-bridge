c811fadddcd960a872b487849e4d7824e8040c51
JaeUpSu <hyeongs2323@gmail.com>
2022-11-24 17:56:05 +0900

    test(validate): 사용하지 않는 import 삭제

--- src/test/java/bridge/validate/IsNumberTest.java
+++ src/test/java/bridge/validate/IsNumberTest.java
@@ -1,15 +1,10 @@
 package bridge.validate;
 
-import bridge.Application;
 import bridge.exception.InvalidIsNumberException;
-import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.util.Lists.newArrayList;
 
 class IsNumberTest {
 
