afe0dc88ac582c7da54357ebfebcec89163ae870
JaeUpSu <hyeongs2323@gmail.com>
2022-11-24 15:37:02 +0900

    feat(view): InputView 각 메서드 입력값 받는 기능 정의

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,28 +1,41 @@
 package bridge.view;
 
+
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private static final String ENTER_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
+    private static final String ENTER_MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String ENTER_GAME_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(ENTER_BRIDGE_SIZE_MESSAGE);
+        int inputValue = Integer.valueOf(Console.readLine());
+        return inputValue;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(ENTER_MOVING_MESSAGE);
+        String inputValue = Console.readLine();
+        return inputValue;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(ENTER_GAME_COMMAND_MESSAGE);
+        String inputValue = Console.readLine();
+        return inputValue;
     }
 }
