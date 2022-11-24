2b674475c81bb4587ae002b5bbeeeb3b1d980fc6
JaeUpSu <hyeongs2323@gmail.com>
2022-11-24 15:53:12 +0900

    refactor(view): InputView 메시지 변수 네이밍 수정

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -8,15 +8,15 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
 
-    private static final String ENTER_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
-    private static final String ENTER_MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
-    private static final String ENTER_GAME_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String READ_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
+    private static final String READ_MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String READ_GAME_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        System.out.println(ENTER_BRIDGE_SIZE_MESSAGE);
+        System.out.println(READ_BRIDGE_SIZE_MESSAGE);
         int inputValue = Integer.valueOf(Console.readLine());
         return inputValue;
     }
@@ -25,7 +25,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        System.out.println(ENTER_MOVING_MESSAGE);
+        System.out.println(READ_MOVING_MESSAGE);
         String inputValue = Console.readLine();
         return inputValue;
     }
@@ -34,7 +34,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        System.out.println(ENTER_GAME_COMMAND_MESSAGE);
+        System.out.println(READ_GAME_COMMAND_MESSAGE);
         String inputValue = Console.readLine();
         return inputValue;
     }
