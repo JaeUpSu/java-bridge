c9357c392104754e15f4159a287878c7c31dbd02
JaeUpSu <hyeongs2323@gmail.com>
2022-11-25 21:00:56 +0900

    feat: 게임 재시작 기능 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -74,5 +74,15 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        this.printingBridge = new PrintingBridge();
+        position = 0;
+        tryCount++;
+    }
+
+    public void updateRetry(String retry) {
+        if (retry.equals(GameCommand.RETRY.getType())) {
+            result = Result.CONTINUE;
+            retry();
+        }
     }
 }
