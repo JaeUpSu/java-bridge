142d6a2b34adb810c19f77f3e7f82366b1f2e9d1
JaeUpSu <hyeongs2323@gmail.com>
2022-11-25 19:28:48 +0900

    feat(domain): Bridge 그리기 클래스 정의

---
+++ src/main/java/bridge/domain/PrintingBridge.java
@@ -0,0 +1,24 @@
+package bridge.domain;
+
+import java.util.List;
+import java.util.StringJoiner;
+
+public class PrintingBridge {
+
+    StringJoiner upBridge;
+    StringJoiner downBridge;
+
+    public PrintingBridge() {
+        this.upBridge = new StringJoiner(" | ", "[ ", " ]");
+        this.downBridge = new StringJoiner(" | ", "[ ", " ]");
+    }
+
+    public void addMoveBridge(String userMove, MovingType computerMoveType) {
+        upBridge.add(MoveAble.getMoveBridgeFilter(userMove, MovingType.UP, computerMoveType));
+        downBridge.add(MoveAble.getMoveBridgeFilter(userMove, MovingType.DOWN, computerMoveType));
+    }
+
+    public List<String> getBridgeMap() {
+        return List.of(upBridge.toString(), downBridge.toString());
+    }
+}
