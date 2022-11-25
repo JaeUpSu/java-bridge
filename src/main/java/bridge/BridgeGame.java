ae2ecdff8d882cef6f1d5844900e0224433430cd
JaeUpSu <hyeongs2323@gmail.com>
2022-11-25 20:50:11 +0900

    feat: 게임 실패 여부 확인 기능 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -40,6 +40,11 @@ public class BridgeGame {
     public Boolean isContinue() {
         return result.equals(Result.CONTINUE);
     }
+
+    public Boolean isFail() {
+        return result.equals(Result.FAIL);
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
