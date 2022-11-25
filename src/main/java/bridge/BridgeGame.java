c1b2adfe7dd6fd422cd3bd3a7e344571e102276b
JaeUpSu <hyeongs2323@gmail.com>
2022-11-25 20:53:31 +0900

    feat: User Bridge 이동 기능 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,8 +1,6 @@
 package bridge;
 
-import bridge.domain.Bridge;
-import bridge.domain.PrintingBridge;
-import bridge.domain.Result;
+import bridge.domain.*;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -54,7 +52,9 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String move) {
+        printingBridge.addMoveBridge(move, MovingType.getMoving(bridge.get(position)));
+        position++;
     }
 
     /**
