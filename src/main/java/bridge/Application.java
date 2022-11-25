cde26bb9a5c1e910355cd4df65b3c34b89afd317
JaeUpSu <hyeongs2323@gmail.com>
2022-11-25 21:18:49 +0900

    feat: 다리 건너기 실행 코드 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,16 @@
 package bridge;
 
+import bridge.controller.BridgeGameController;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            BridgeGameController controller = new BridgeGameController();
+            controller.start();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
