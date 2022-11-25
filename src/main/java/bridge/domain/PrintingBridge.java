3cf5dd7be41c38c1a0b0cf1770fbf087c200c9e0
JaeUpSu <hyeongs2323@gmail.com>
2022-11-25 20:04:29 +0900

    feat(domain): 게임 계속될 수 있는지 확인 기능 정의

--- src/main/java/bridge/domain/PrintingBridge.java
+++ src/main/java/bridge/domain/PrintingBridge.java
@@ -1,24 +1,41 @@
 package bridge.domain;
 
-import java.util.List;
 import java.util.StringJoiner;
 
 public class PrintingBridge {
 
     StringJoiner upBridge;
     StringJoiner downBridge;
+    Boolean continueGame;
 
     public PrintingBridge() {
         this.upBridge = new StringJoiner(" | ", "[ ", " ]");
         this.downBridge = new StringJoiner(" | ", "[ ", " ]");
+        this.continueGame = true;
+    }
+
+    public Boolean isContinueGame() {
+        return continueGame;
     }
 
     public void addMoveBridge(String userMove, MovingType computerMoveType) {
-        upBridge.add(MoveAble.getMoveBridgeFilter(userMove, MovingType.UP, computerMoveType));
-        downBridge.add(MoveAble.getMoveBridgeFilter(userMove, MovingType.DOWN, computerMoveType));
+        String nextUpState = MoveAble.getMoveBridgeFilter(userMove, MovingType.UP, computerMoveType);
+        String nextDownState = MoveAble.getMoveBridgeFilter(userMove, MovingType.DOWN, computerMoveType);
+
+        upBridge.add(nextUpState);
+        downBridge.add(nextDownState);
+        checkContinueGame(nextUpState, nextDownState);
     }
 
     public String getBridgeMap() {
-        return upBridge.toString() + "\n" +  downBridge.toString();
+        return upBridge.toString() + "\n" + downBridge.toString();
+    }
+
+    private void checkContinueGame(String nextUpState, String nextDownState) {
+        String disAllowance = MoveAble.DIS_ALLOWANCE.getStatus();
+
+        if (nextUpState.contains(disAllowance) || nextDownState.contains(disAllowance)) {
+            continueGame = false;
+        }
     }
 }
