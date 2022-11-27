808bb8a5f164c8c8f086dae6f23c5272a1200afc
JaeUpSu <hyeongs2323@gmail.com>
2022-11-27 20:18:25 +0900

    feat(OutputView): 이동한 다리의 상태를 정해진 형식에 맞춰 출력 기능 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,6 +1,10 @@
 package bridge.view;
 
+import bridge.domain.MoveStatus;
+
 import java.text.MessageFormat;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -9,13 +13,25 @@ public class OutputView {
 
     private static final String GAME_START_MESSAGE = "\n다리 건너기 게임을 시작합니다.";
     private static final String ERROR_MESSAGE_FORMAT = "\n[ERROR] {0}";
+    private static final String MAP_MESSAGE_FORMAT = "[ {0} ]";
+    private static final String MAP_MESSAGE_DELIMITER = " | ";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<List<MoveStatus>> result) {
+        for (List<MoveStatus> statuses : result) {
+            System.out.println(getResultMessage(statuses));
+        }
+    }
+
+    private String getResultMessage(List<MoveStatus> statuses) {
+        return String.format(MAP_MESSAGE_FORMAT, statuses.stream()
+                .map(MoveStatus::get)
+                .collect(Collectors.joining(MAP_MESSAGE_DELIMITER))
+        );
     }
 
     /**
